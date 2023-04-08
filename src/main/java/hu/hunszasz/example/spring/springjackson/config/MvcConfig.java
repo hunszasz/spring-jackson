f84e4a5cf23aca25cba41bae64267654e62f5767
szaszgk <Szasz2830023>
2023-04-09 01:06:18 +0200

    step 10/10

---
+++ src/main/java/hu/hunszasz/example/spring/springjackson/config/MvcConfig.java
@@ -0,0 +1,27 @@
+package hu.hunszasz.example.spring.springjackson.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    private ObjectMapper objectMapper;
+
+    public MvcConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream()
+                .filter(MappingJackson2HttpMessageConverter.class::isInstance)
+                .map(c -> (MappingJackson2HttpMessageConverter) c)
+                .forEach(c -> c.setObjectMapper(objectMapper));
+    }
+}
