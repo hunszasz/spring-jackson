cc1ec1e0c4fac19f7718cd58e7eea7a8ec7f7668
szaszgk <Szasz2830023>
2023-04-08 22:56:17 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Jackson Demo Config
+
+[hunszasz.github.io kapcsolódó blog bejegyzés](https://hunszasz.github.io/fejleszt%C5%91i%20k%C3%B6rnyezet/hasznos/trend/2023/04/04/jackson-bemutat%C3%A1sa-spring-keretrendszerben-10-l%C3%A9p%C3%A9sben.html)
---
+++ src/test/java/hu/hunszasz/example/spring/springjackson/SpringJacksonDemoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.hunszasz.example.spring.springjackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJacksonDemoConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
